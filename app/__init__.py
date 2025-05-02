51f42a9a1035f3a75fcf8b1b92349180bdb92584
Junt Surani <junitsurani@gmail.com>
2025-05-02 15:57:33 +0530

    Added Config.

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_mail import Mail
 from .routes.home import home
@@ -12,6 +13,15 @@ def create_app():
     app = Flask(__name__)
     CORS(app, resources={r"/*": {"origins": "*"}})
     app.config.from_object('app.config.Config')
+
+    app.config.update(
+        MAIL_SERVER = os.getenv('SMTP_HOST', 'smtp.gmail.com'),
+        MAIL_PORT = os.getenv('SMTP_PORT', 587),
+        MAIL_USE_TLS = True,
+        MAIL_USERNAME = os.getenv('SMTP_USER'),
+        MAIL_PASSWORD = os.getenv('SMTP_PASSWORD'),
+        MAIL_DEFAULT_SENDER = os.getenv('SMTP_USER'),
+    )
 
     mail.init_app(app)
 
